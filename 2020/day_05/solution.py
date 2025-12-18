6b68029c8dcde1d4bece15a68d97815614f93fd1
tjhrad <14080006+tjhrad@users.noreply.github.com>
2025-12-17 18:31:31 -0700

    day 5

--- 2020/day_05/solution.py
+++ 2020/day_05/solution.py
@@ -6,13 +6,50 @@ from helpers import format_time, read_input
 
 
 def part1(data):
-    answer = 0
+    answer = max([(find_middle(line[:7], 0, 127) * 8) + find_middle(line[-3:], 0, 7) for line in data])
     print(f"\nPart 1: {answer}")
 
 
 def part2(data):
-    answer = 0
+    all_ids = get_ids()
+    taken_ids = [(find_middle(line[:7], 0, 127) * 8) + find_middle(line[-3:], 0, 7) for line in data]
+    answer = find_seat(all_ids, taken_ids)
     print(f"\nPart 2: {answer}")
+
+
+def find_seat(all_ids, taken_ids):
+    for i in range(len(all_ids)):
+        if all_ids[i] in taken_ids:
+            continue
+
+        if all_ids[i-1] not in taken_ids:
+            continue
+
+        if all_ids[i+1] not in taken_ids:
+            continue
+
+        return all_ids[i]
+    return None
+
+
+def find_middle(s, min_n, max_n):
+    for ch in s:
+        if ch in 'FL':
+            max_n = (min_n + max_n) // 2
+        elif ch in 'BR':
+            min_n = (min_n + max_n) // 2
+        else:
+            print("Error: unknown char: {ch}")
+            return None
+    return max_n
+
+
+def get_ids():
+    ids = []
+    for r in range(1, 126):
+        for c in range(0, 7):
+            ids.append((r*8) + c)
+    return ids
 
 
 def main():
